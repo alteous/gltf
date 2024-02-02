c40b32c41e9ead527a21ff8e4464c450117e4549
David Harvey-Macaulay <david@harvey-macaulay.com>
2024-02-02 12:48:15 +0000

    Remove json::validation::Checked enum

--- gltf-json/src/validation.rs
+++ gltf-json/src/validation.rs
@@ -1,4 +1,3 @@
-use serde::{ser, Serialize, Serializer};
 use std::collections::BTreeMap;
 use std::hash::Hash;
 
@@ -30,80 +29,6 @@ pub enum Error {
 
     /// A memory size or offset exceeds the system limits.
     Oversize,
-}
-
-/// Specifies a type that has been pre-validated during deserialization or otherwise.
-#[derive(Debug, Eq, Hash, PartialEq, Ord, PartialOrd)]
-pub enum Checked<T> {
-    /// The item is valid.
-    Valid(T),
-
-    /// The item is invalid.
-    Invalid,
-}
-
-impl<T> Checked<T> {
-    /// Converts from `Checked<T>` to `Checked<&T>`.
-    pub fn as_ref(&self) -> Checked<&T> {
-        match *self {
-            Checked::Valid(ref item) => Checked::Valid(item),
-            Checked::Invalid => Checked::Invalid,
-        }
-    }
-
-    /// Takes ownership of the contained item if it is `Valid`.
-    ///
-    /// # Panics
-    ///
-    /// Panics if called on an `Invalid` item.
-    pub fn unwrap(self) -> T {
-        match self {
-            Checked::Valid(item) => item,
-            Checked::Invalid => panic!("attempted to unwrap an invalid item"),
-        }
-    }
-}
-
-impl<T: Serialize> Serialize for Checked<T> {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: Serializer,
-    {
-        match *self {
-            Checked::Valid(ref item) => item.serialize(serializer),
-            Checked::Invalid => Err(ser::Error::custom("invalid item")),
-        }
-    }
-}
-
-impl<T: Clone> Clone for Checked<T> {
-    fn clone(&self) -> Self {
-        match *self {
-            Checked::Valid(ref item) => Checked::Valid(item.clone()),
-            Checked::Invalid => Checked::Invalid,
-        }
-    }
-}
-
-impl<T: Copy> Copy for Checked<T> {}
-
-impl<T: Default> Default for Checked<T> {
-    fn default() -> Self {
-        Checked::Valid(T::default())
-    }
-}
-
-impl<T> Validate for Checked<T> {
-    fn validate<P, R>(&self, _root: &Root, path: P, report: &mut R)
-    where
-        P: Fn() -> Path,
-        R: FnMut(&dyn Fn() -> Path, Error),
-    {
-        match *self {
-            Checked::Valid(_) => {}
-            Checked::Invalid => report(&path, Error::Invalid),
-        }
-    }
 }
 
 /// Validates the suitability of 64-bit byte offsets/sizes on 32-bit systems.
